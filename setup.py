040ea8f62c803a7db6f51616cbf852f6fde836df
moritzkoerber <koerber.moritz@gmail.com>
2021-06-13 19:30:30 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+from setuptools import setup
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+
+def get_version(rel_path: str):
+    file_path = pathlib.Path(__file__).parent.absolute() / rel_path
+    for line in file_path.read_text().splitlines():
+        if line.startswith("__version__"):
+            return line.split('"')[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="binary4fun",
+    version=get_version("binary4fun/__init__.py"),
+    url="github...",
+    author="Moritz Körber",
+    author_email="moritz.koerber@gmail.com",
+    description="A little game to guess numbers by means of binary search.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["binary4fun"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
